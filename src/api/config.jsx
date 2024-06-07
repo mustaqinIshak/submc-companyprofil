2e05b994822cf2857e0db13422acf4a9ee100551
mustaqinIshak <mustaqinishak@gmail.com>
2024-06-07 11:52:54 +0800

    change domain server

--- src/api/config.jsx
+++ src/api/config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://api.amakbars.com/api',
+    baseURL: 'https://api.crasherltd.com/api',
     timeout: 5000,
   });
 
